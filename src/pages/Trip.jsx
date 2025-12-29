23d7f3fcbfa72a68dd8647a1c3fdc4fde559e089
davud97 <dawoodilyas48@gmail.com>
2025-12-29 20:15:04 +0300

    made new function

--- src/pages/Trip.jsx
+++ src/pages/Trip.jsx
@@ -1,3 +1,13 @@
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { GetAllPlans } from "../services/tripPlanServices"
+const Trip = () => {
+  const navigate = useNavigate()
+  const [plans, setPlans] = useState([])
+  useEffect(() => {
+    const getPlans = async () => {
+      const plans = await GetAllPlans()
+      setPlans(plans)
+    }
+    getPlans()
+  }, [])
