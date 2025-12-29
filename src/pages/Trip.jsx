eb9f12ee3dd1042a506f9c77d741b2cd5b675b52
davud97 <dawoodilyas48@gmail.com>
2025-12-29 20:18:02 +0300

    there was typo in line 13, fixed it and finalized the work in trip.jsx

--- src/pages/Trip.jsx
+++ src/pages/Trip.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { GetAllPlans } from "../services/tripPlanServices"
+
 const Trip = () => {
   const navigate = useNavigate()
   const [plans, setPlans] = useState([])
@@ -11,3 +12,29 @@ const Trip = () => {
     }
     getPlans()
   }, [])
+
+  return (
+    <>
+      <div>
+        <h1>Explore People's Plans</h1>
+        {plans.map((plan) => (
+          <div>
+            <h3>Day {plan.day}</h3>
+            <p>
+              <strong>City:</strong> {plan.attraction.city}
+            </p>
+            <p>
+              <strong>Country:</strong> {plan.attraction.country}
+            </p>
+            <p>
+              <strong>Notes:</strong> {plan.notes}
+            </p>
+            <img src={plan.attraction.picture} alt="attraction" />
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default Trip
