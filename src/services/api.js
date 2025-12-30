280fd66147a787ab9423a0b8935c77caba3fe831
davud97 <dawoodilyas48@gmail.com>
2025-12-30 16:30:33 +0300

    fixed api.js and auth.js too

--- src/services/api.js
+++ src/services/api.js
@@ -1,29 +1,28 @@
-import Client from "./api"
+import axios from "axios"
 
-export const RegisterUser = async (data) => {
-  try {
-    const res = await Client.post("/auth/register", data)
-    return res.data
-  } catch (error) {
+export const BASE_URL = "http://localhost:3000/"
+
+const Client = axios.create({
+  baseURL: BASE_URL
+})
+
+// Intercepts every request axios makes
+Client.interceptors.request.use(
+  async (config) => {
+    // Reads the token in localStorage
+    const token = localStorage.getItem("token")
+    // if the token exists, we set the authorization header
+    if (token) {
+      config.headers["authorization"] = `Bearer ${token}`
+
+    }
+    // We return the new config if the token exists or the default config if no token exists.
+    return config
+  },
+  async (error) => {
+    console.log({ msg: "Axios Interceptor Error!", error })
     throw error
   }
-}
+)
 
-export const SignInUser = async (data) => {
-  try {
-    const res = await Client.post("/auth/login", data)
-    localStorage.setItem('token', res.data.token)
-    return res.data.user
-  } catch (error) {
-    throw error
-  }
-}
-
-export const CheckSession = async () => {
-  try {
-    const res = await Client.get("/auth/session")
-    return res.data
-  } catch (error) {
-    throw error
-  }
-}
+export default Client
