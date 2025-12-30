26a742a3b5d7b56c0653b1af0746a158802fbfd0
davud97 <dawoodilyas48@gmail.com>
2025-12-30 16:14:14 +0300

    added jwt token

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,20 @@
-import axios from "axios"
+import Client from "./api"
 
-export const BASE_URL = 'http://localhost:3000'
- 
-const Client = axios.create({ baseURL: BASE_URL })
+export const RegisterUser = async (data) => {
+  try {
+    const res = await Client.post("/auth/register", data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
 
-export default Client
+export const SignInUser = async (data) => {
+  try {
+    const res = await Client.post("/auth/login", data)
+    localStorage.setItem('token', res.data.token)
+    return res.data.user
+  } catch (error) {
+    throw error
+  }
+}
