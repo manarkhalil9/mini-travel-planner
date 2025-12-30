a75ed6c7372c96b710cf8af239b25926582b9c66
manarkhalil9 <manarkhalil9@users.noreply.github.com>
2025-12-30 17:54:11 +0300

    place btn

--- src/components/AttractionPopup.jsx
+++ src/components/AttractionPopup.jsx
@@ -4,13 +4,14 @@ const AttractionPopup = ({ attraction, onClose, addToTrip }) => {
   return (
     <div className="popup">
       <div className="popup-content">
-        <button onClick={onClose}>Close</button>
 
         <h2>{attraction.name}</h2>
         <p>{attraction.description}</p>
         <p>City: {attraction.city}</p>
         <p>Country: {attraction.country}</p>
         <img src={attraction.picture} alt={attraction.name} />
+
+        <button onClick={onClose}>Close</button>
 
         {addToTrip && (
           <button onClick={() => addToTrip(attraction)}>
