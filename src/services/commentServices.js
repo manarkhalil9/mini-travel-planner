f471ddbc1dd6ab9853f24e834ffc06b6c138c5a6
davud97 <dawoodilyas48@gmail.com>
2025-12-30 16:16:15 +0300

    fixed the client route in each function

--- src/services/commentServices.js
+++ src/services/commentServices.js
@@ -8,3 +8,39 @@ export const GetComments = async (tripId) => {
     throw error
   }
 }
+
+export const GetCommentById = async (tripId, commentId) => {
+  try {
+    const res = await Client.get(/trip/${tripId}/comment/${commentId})
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const CreateComment = async (tripId, data) => {
+  try {
+    const res = await Client.post(/trip/${tripId}/comment, data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const UpdateComment = async (tripId, commentId, data) => {
+  try {
+    const res = await Client.put(/trip/${tripId}/comment/${commentId}, data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const DeleteComment = async (tripId, commentId) => {
+  try {
+    const res = await Client.delete(/trip/${tripId}/comment/${commentId})
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
